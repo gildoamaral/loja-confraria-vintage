0237da962e4fa794060d251bf06e2548f65911d3
Marco Antonio Gil <gildo_marco@hotmail.com>
2025-03-22 09:59:22 -0300

    adicionando logica de login e verificando erro no DB

---
+++ codigo-fonte/src/routes/Login.js
@@ -0,0 +1,29 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+require("dotenv").config();
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Verifica se o usuário existe
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) return res.status(400).json({ message: "E-mail ou senha inválidos" });
+
+    // Compara as senhas
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) return res.status(400).json({ message: "E-mail ou senha inválidos" });
+
+    // Gera o token JWT
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+    res.status(200).json({ msg: "Autenticação realizada com sucesso", token });
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao fazer login" });
+  }
+};
+
+module.exports = login ;
