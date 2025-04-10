457f8c0c2942d87c2cbca1634cb7dbe61f4e6f5b
Marco Antonio Gil <gildo_marco@hotmail.com>
2025-04-10 00:49:45 -0300

    começando a padronizar o frontEnd

--- codigo-fonte/Front/src/App.jsx
+++ codigo-fonte/Front/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/navbar.jsx";
 import AppRoutes from "./routes/AppRoutes.jsx";
+import logo from "./assets/image-Photoroom.png";
 
 function App() {
   return (
     <BrowserRouter>
+      <img src={logo} alt="Logo" style={{ width: "40em", margin: "20px" }} />
       <Navbar />  {}
       <AppRoutes />
     </BrowserRouter>
