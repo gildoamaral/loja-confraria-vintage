42c33a0d32e81681b294111844d049a309b62bea
Lucas Soares <lucsoares03@gmail.com>
2025-04-23 21:34:01 -0300

    Atualizações login / cadastro de usuários

--- codigo-fonte/Front/src/pages/Cadastro/CadastroUsuario.jsx
+++ codigo-fonte/Front/src/pages/Cadastro/CadastroUsuario.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import Styles from '../Login/Login.module.css';
+import Header from '../../components/Header';
 import Footer from '../../components/Footer';  
 
 const CadastroUsuario = () => {
@@ -19,7 +20,6 @@ const CadastroUsuario = () => {
     setIsSubmitting(true);
 
     try {
-
       await axios.post('http://localhost:3000/usuarios', {
         nome,
         sobrenome,
@@ -47,54 +47,57 @@ const CadastroUsuario = () => {
   };
 
     return (
-        <div className={Styles.container}>
-            <div className={Styles.header}>
-                {/* <img src={logo} alt="Logo AutoCare" className={styles.logo} /> */}
-                <h1>Crie agora sua conta na <span className={Styles.highlight}>Confraria Vintage</span></h1>
-                <h3>e redescubra sua feminilidade!</h3>
-            </div>
-            <div className={Styles.formContainer}>
+        <div>
+            <Header />
+            <div className={Styles.container}>
+                <div className={Styles.header}>
+                    {/* <img src={logo} alt="Logo AutoCare" className={styles.logo} /> */}
+                    <h1>Crie agora sua conta na <span className={Styles.highlight}>Confraria Vintage</span></h1>
+                    <h3>e redescubra sua feminilidade!</h3>
+                </div>
+                <div className={Styles.formContainer}>
 
-            <h2>Cadastre-se</h2>
-                <form onSubmit={handleSubmit}>
-                    <div className={Styles.inputGroup}>
-                        <label>Nome:</label>
-                        <input type="text" value={nome} onChange={(e) => setNome(e.target.value)} required />
-                    </div>
-                    <div className={Styles.inputGroup}>
-                        <label>Sobrenome:</label>
-                        <input type="text" value={sobrenome} onChange={(e) => setSobrenome(e.target.value)} required />
-                    </div>
-                    <div className={Styles.inputGroup}>
-                        <label>Data de nascimento:</label>
-                        <input type="date" value={dataNascimento} onChange={(e) => setDataNascimento(e.target.value)} />
-                    </div>
-                    <div className={Styles.inputGroup}>
-                        <label>Endereço:</label>
-                        <input type="text" value={endereco} onChange={(e) => setEndereco(e.target.value)} />
-                    </div>
-                    <div className={Styles.inputGroup}>
-                        <label>E-mail:</label>
-                        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-                    </div>
-                    <div className={Styles.inputGroup}>
-                        <label>Telefone:</label>
-                        <input type="text" value={telefone} onChange={(e) => setTelefone(e.target.value)} />
-                    </div>
-                    <div className={Styles.inputGroup}>
-                        <label>Senha:</label>
-                        <input type="password" value={senha} onChange={(e) => setSenha(e.target.value)} required />
-                    </div>
-                    
-                    <button type="submit" disabled={isSubmitting} className={Styles.button}>
-                    {isSubmitting ? 'Enviando...' : 'Cadastrar'}
-                    </button>
+                <h2>Cadastre-se</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className={Styles.inputGroup}>
+                            <label>Nome:</label>
+                            <input type="text" value={nome} onChange={(e) => setNome(e.target.value)} required />
+                        </div>
+                        <div className={Styles.inputGroup}>
+                            <label>Sobrenome:</label>
+                            <input type="text" value={sobrenome} onChange={(e) => setSobrenome(e.target.value)} required />
+                        </div>
+                        <div className={Styles.inputGroup}>
+                            <label>Data de nascimento:</label>
+                            <input type="date" value={dataNascimento} onChange={(e) => setDataNascimento(e.target.value)} />
+                        </div>
+                        <div className={Styles.inputGroup}>
+                            <label>Endereço:</label>
+                            <input type="text" value={endereco} onChange={(e) => setEndereco(e.target.value)} />
+                        </div>
+                        <div className={Styles.inputGroup}>
+                            <label>E-mail:</label>
+                            <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        </div>
+                        <div className={Styles.inputGroup}>
+                            <label>Telefone:</label>
+                            <input type="text" value={telefone} onChange={(e) => setTelefone(e.target.value)} />
+                        </div>
+                        <div className={Styles.inputGroup}>
+                            <label>Senha:</label>
+                            <input type="password" value={senha} onChange={(e) => setSenha(e.target.value)} required />
+                        </div>
+                        
+                        <button type="submit" disabled={isSubmitting} className={Styles.button}>
+                        {isSubmitting ? 'Enviando...' : 'Cadastrar'}
+                        </button>
 
-                    <p>Já tem uma conta? <a href="/login" className={Styles.signupLink}>Faça login</a></p>
-                </form>
-                {message && <p style={{ color: message.includes('sucesso') ? 'green' : 'red' }}>{message}</p>}
-            </div>
+                        <p>Já tem uma conta? <a href="/login" className={Styles.signupLink}>Faça login</a></p>
+                    </form>
+                    {message && <p style={{ color: message.includes('sucesso') ? 'green' : 'red' }}>{message}</p>}
+                </div>
             <Footer />
+            </div>
         </div>
     );
 }
--- codigo-fonte/Front/src/pages/Login/Login.jsx
+++ codigo-fonte/Front/src/pages/Login/Login.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import api from '../../services/api';
 import { useNavigate } from 'react-router-dom';
 import Styles from'./Login.module.css';
+import Header from '../../components/Header';
 import Footer from '../../components/Footer';  
 
 const Login = () => {
@@ -34,43 +35,46 @@ const Login = () => {
     };
 
     return (
-        <div className={Styles.container}>
-            <div className={Styles.header}>
-                <h1>Bem-vindo de volta a <span className={Styles.highlight}>Confraria Vintage</span></h1>
-                <h3>Moda elegante com personalidade e graciosidade</h3>
-            </div>
-            <div className={Styles.formContainer}>
-                <h2>Login</h2>
+        <div>
+            <Header />
+            <div className={Styles.container}>
+                <div className={Styles.header}>
+                    <h1>Bem-vindo de volta a <span className={Styles.highlight}>Confraria Vintage</span></h1>
+                    <h3>Moda elegante com personalidade e graciosidade</h3>
+                </div>
+                <div className={Styles.formContainer}>
+                    <h2>Login</h2>
 
-                <form onSubmit={handleLogin}>
-                    <div className={Styles.inputGroup}>
-                        <label htmlFor="email">E-mail</label>
-                        <input 
-                            id="email" 
-                            type="email" 
-                            placeholder="E-mail" 
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                            />
-                    </div>
-                    <div className={Styles.inputGroup}>
-                        <label htmlFor="senha">Senha</label>
-                        <input 
-                            id="senha" 
-                            type="password" 
-                            placeholder="Senha"
-                            value={senha} 
-                            onChange={(e) => setSenha(e.target.value)}
-                            required
-                            />
-                    </div>
-                    <button type="submit" className={Styles.button}>LOGIN</button>
-                </form>
+                    <form onSubmit={handleLogin}>
+                        <div className={Styles.inputGroup}>
+                            <label htmlFor="email">E-mail</label>
+                            <input 
+                                id="email" 
+                                type="email" 
+                                placeholder="E-mail" 
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                                />
+                        </div>
+                        <div className={Styles.inputGroup}>
+                            <label htmlFor="senha">Senha</label>
+                            <input 
+                                id="senha" 
+                                type="password" 
+                                placeholder="Senha"
+                                value={senha} 
+                                onChange={(e) => setSenha(e.target.value)}
+                                required
+                                />
+                        </div>
+                        <button type="submit" className={Styles.button}>LOGIN</button>
+                    </form>
 
-                {loginSucesso && <p className={Styles.successMessage}>Login realizado com sucesso!</p>}
-                {erroLogin && <p className={Styles.errorMessage}>{erroLogin}</p>}
-                <p>Ainda não possui uma conta? <a href="/cadastro-usuario" className={Styles.signupLink}>Cadastre-se</a></p>
+                    {loginSucesso && <p className={Styles.successMessage}>Login realizado com sucesso!</p>}
+                    {erroLogin && <p className={Styles.errorMessage}>{erroLogin}</p>}
+                    <p>Ainda não possui uma conta? <a href="/cadastro-usuario" className={Styles.signupLink}>Cadastre-se</a></p>
+                </div>
             </div>
             <Footer />
         </div>
