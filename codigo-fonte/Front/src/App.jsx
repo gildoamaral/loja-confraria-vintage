916dad3811f367f5956055ce9d68eb7ceccb003d
Marco Antonio Gil <gildo_marco@hotmail.com>
2025-05-29 02:57:58 -0300

    .

--- codigo-fonte/Front/src/App.jsx
+++ codigo-fonte/Front/src/App.jsx
@@ -2,8 +2,6 @@ import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes.jsx";
 import WhatsAppButton from "./components/WhatsAppButton";  // Importando o botão do WhatsApp
 import { CarrinhoProvider } from './context/CarrinhoContext.jsx';
-import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
 
 
 function App() {
@@ -11,11 +9,9 @@ function App() {
     <CarrinhoProvider>
       <BrowserRouter>
 
-        {/* <Header /> */}
 
         <AppRoutes />
 
-        {/* <Footer /> */}
 
 
         <WhatsAppButton /> {/* Adicionando o botão flutuante do WhatsApp */}
