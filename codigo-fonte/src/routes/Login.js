c0ad1e075bb71cbc8e815dda954b37621e59ccfb
Marco Antonio Gil <gildo_marco@hotmail.com>
2025-06-02 22:03:15 -0300

    testando mudança no cookie

--- codigo-fonte/src/routes/Login.js
+++ codigo-fonte/src/routes/Login.js
@@ -16,22 +16,22 @@ const login = async (req, res) => {
     // Compara as senhas
     const validPassword = await bcrypt.compare(senha, user.senha);
     if (!validPassword) return res.status(400).json({ message: "E-mail ou senha inválidos" });
-  
+
     // Gera o token JWT
     const token = jwt.sign(
-      { userId: user.id, posicao: user.posicao }, 
+      { userId: user.id, posicao: user.posicao },
       process.env.JWT_SECRET,
       { expiresIn: "1h" }
     );
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false, // só funciona com HTTPS
-      sameSite: "Strict", // ou "Lax" se tiver domínio diferente
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Strict",
       maxAge: 60 * 60 * 1000 // 1 hora
     });
 
-    res.status(200).json({ msg: "Autenticação realizada com sucesso"});
+    res.status(200).json({ msg: "Autenticação realizada com sucesso" });
 
   } catch (error) {
     res.status(500).json({ error: "Erro ao fazer login" });
