912bd7790753fbe38bbd9065b53fa7171760e8c5
Marco Antonio Gil <gildo_marco@hotmail.com>
2025-06-12 16:28:40 -0300

    correção do carrossel no celular

--- codigo-fonte/Front/src/components/Carrossel/Carrossel.jsx
+++ codigo-fonte/Front/src/components/Carrossel/Carrossel.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Slider from 'react-slick'
-import { IconButton } from '@mui/material'
+import { IconButton, useMediaQuery } from '@mui/material'
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew'
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
 import 'slick-carousel/slick/slick.css'
@@ -10,55 +10,85 @@ import img1 from './Gen1.png'
 import img2 from './Gen2.png'
 import img3 from './Gen3.png'
 
-const images = [
-<img src={img1} alt={`slide-1`} style={{width: '100%', height: '100vh', objectFit: 'cover',
-objectPosition: "0 5%"
-}}/>, 
-<img src={img2} alt={`slide-2`} style={{width: '100%', height: '100vh', objectFit: 'cover', objectPosition: "0 40%"}}/>, 
-<img src={img3} alt={`slide-3`} style={{width: '100%', height: '100vh', objectFit: 'cover', objectPosition: "0 40%"}}/>
-]
 
-function NextArrow(props) {
-  const { onClick } = props
-  return (
-    <IconButton
-      onClick={onClick}
-      sx={{
-        position: 'absolute',
-        top: '50%',
-        right: 16,
-        zIndex: 1,
-        color: 'white',
-        backgroundColor: 'rgba(0,0,0,0.3)',
-        '&:hover': { backgroundColor: 'rgba(0,0,0,0.5)' }
-      }}
-    >
-      <ArrowForwardIosIcon />
-    </IconButton>
-  )
-}
-
-function PrevArrow(props) {
-  const { onClick } = props
-  return (
-    <IconButton
-      onClick={onClick}
-      sx={{
-        position: 'absolute',
-        top: '50%',
-        left: 16,
-        zIndex: 1,
-        color: 'white',
-        backgroundColor: 'rgba(0,0,0,0.3)',
-        '&:hover': { backgroundColor: 'rgba(0,0,0,0.5)' }
-      }}
-    >
-      <ArrowBackIosNewIcon />
-    </IconButton>
-  )
-}
 
 const Carrossel = () => {
+  const isMobile = useMediaQuery('(max-width:600px)')
+
+  const images =
+    [
+      <img src={img1} alt={`slide-1`} style=
+        {
+          {
+            width: '100%',
+            height: '100vh',
+            objectFit: 'cover',
+        objectPosition: isMobile ? 'center' : '0 5%',
+          }
+        }
+      />,
+      <img src={img2} alt={`slide-2`} style=
+        {
+          {
+            width: '100%',
+            height: '100vh',
+            objectFit: 'cover',
+        objectPosition: isMobile ? 'center' : '0 40%',
+          }
+        }
+      />,
+      <img src={img3} alt={`slide-3`} style=
+        {
+          {
+            width: '100%',
+            height: '100vh',
+            objectFit: 'cover',
+        objectPosition: isMobile ? '10% 0' : '0 40%',
+          }
+        }
+      />
+    ]
+
+  function NextArrow(props) {
+    const { onClick } = props
+    return (
+      <IconButton
+        onClick={onClick}
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          right: 16,
+          zIndex: 1,
+          color: 'white',
+          backgroundColor: 'rgba(0,0,0,0.3)',
+          '&:hover': { backgroundColor: 'rgba(0,0,0,0.5)' }
+        }}
+      >
+        <ArrowForwardIosIcon />
+      </IconButton>
+    )
+  }
+
+  function PrevArrow(props) {
+    const { onClick } = props
+    return (
+      <IconButton
+        onClick={onClick}
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          left: 16,
+          zIndex: 1,
+          color: 'white',
+          backgroundColor: 'rgba(0,0,0,0.3)',
+          '&:hover': { backgroundColor: 'rgba(0,0,0,0.5)' }
+        }}
+      >
+        <ArrowBackIosNewIcon />
+      </IconButton>
+    )
+  }
+
   const settings = {
     dots: true,
     infinite: true,
