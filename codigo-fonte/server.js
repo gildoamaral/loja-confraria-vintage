fe5d06f047706d47fde9a650c413a88190bbb1d7
Marco Antonio Gil <gildo_marco@hotmail.com>
2025-07-19 10:37:44 -0300

    tentativa de deploy

--- codigo-fonte/server.js
+++ codigo-fonte/server.js
@@ -164,3 +164,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
+
+module.exports = app; // Exporta o app para testes
